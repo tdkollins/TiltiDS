9514ce3cef2ae41403ce3bf6f03f50a851438dac
Trevor <tdkollin@edu.uwaterloo.ca>
2019-12-05 00:58:21 -0500

    added donation_vs_time

--- scrapper.py
+++ scrapper.py
@@ -6,56 +6,90 @@ import pandas
 import pygsheets
 from datetime import datetime
 
-api_url = 'https://tiltify.com/api/v3/'
-camp_id_list = [""]
+api_url = "https://tiltify.com/api/v3/"
+with open("setup.json", "r") as setup_file:
+    setup = json.loads(setup_file.read())
 
 scope = [
     "https://spreadsheets.google.com/feeds",
-    'https://www.googleapis.com/auth/spreadsheets',
+    "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ]
 
-token = ""
+camp_list = setup["campaignList"]
+token = setup["token"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
+
+def don_vs_time(start, data, df_len):
+    donation_total = []
+    time = start
+    donation_dataframe = pandas.DataFrame(data)
+    donation_dataframe = donation_dataframe.iloc[::-1]
+    # print(donation_dataframe)
+    donation_total = []
+    cutoff = start
+    donation_index = 0
+    total = 0
+    donation_dataframe_len = len(donation_dataframe)
+    don_time = donation_dataframe["completedAt"].iloc[0] // 1000
+
+    for dataframe_index in range(df_len):
+        if (donation_index == donation_dataframe_len):
+            donation_total.append(total)
+            continue
+        while (don_time < cutoff):
+            total += donation_dataframe["amount"].iloc[donation_index]
+            donation_index += 1
+            if (donation_index == donation_dataframe_len):
+                break
+            don_time = donation_dataframe["completedAt"].iloc[donation_index] // 1000
+        cutoff += 1800
+        donation_total.append(total)
+
+    return donation_total
+
+
+def get_hours(duration):
+    hour_col = []
+    for hour in range(duration):
+        hour_col.append("{:02}:00:00".format(hour))
+        hour_col.append("{:02}:30:00".format(hour))
+    hour_col.append("{:02}:00:00".format(duration))
+    return hour_col
 
 
 def tiltify_request(url, params=None):
     response = requests.get(url = url,
-                            headers = {'Authorization': 'Bearer {}'.format(token)},
+                            headers = {"Authorization": "Bearer {}".format(token)},
                             params = params)
     return response
 
 def main():
-    for camp_id in camp_id_list:
-        campaign_url = api_url + 'campaigns/' + camp_id
-        campaign_data = json.loads(tiltify_request(campaign_url).text)['data']
-        campaign_name = campaign_data['name']
-        print("Enter the start time for " + campaign_name + " in epoch timestamp")
-        #start_timestamp = int(input())
-        start_timestamp = 1501502400
-        print("Enter the end time for " + campaign_name + " in epoch timestamp")
-        #end_timestamp = int(input())
-        end_timestamp = 1501801200
+    for camp in camp_list:
+        camp_id = camp["id"]
+        camp_url = api_url + "campaigns/" + camp_id
+        camp_data = json.loads(tiltify_request(camp_url).text)["data"]
+        camp_name = camp_data["name"]
+        start_timestamp = camp["startTimestamp"] # epoch timestamp
+        end_timestamp = camp["endTimestamp"]
         start_datetime = datetime.fromtimestamp(start_timestamp)
         end_datetime = datetime.fromtimestamp(end_timestamp)
         duration = int((end_datetime - start_datetime).total_seconds() // 3600)
+        print("Campaign: {}".format(camp_name))
         print("Started: {}".format(start_datetime))
         print("Ended: {}".format(end_datetime))
         print("Duration in Hours: {}".format(duration))
 
         dataframe = pandas.DataFrame()
-        hour_col = []
-        for hour in range(duration):
-            hour_col.append('{:02}:00:00'.format(hour))
-            hour_col.append('{:02}:30:00'.format(hour))
-        hour_col.append('{:02}:00:00'.format(duration))
-        dataframe['Marathon Time'] = hour_col
+        dataframe["Marathon Time"] = get_hours(duration)
+        # print(dataframe)
 
-        donation_url = api_url + 'campaigns/' + camp_id + '/donations/'
-        donation_data = json.loads(tiltify_request(donation_url, {'count': 1000000}).text)['data']
-        print(pandas.DataFrame(donation_data))
+        donation_url = api_url + "campaigns/" + camp_id + "/donations/"
+        donation_data = json.loads(tiltify_request(donation_url, {"count": 1000000}).text)["data"]
+        dataframe["Donation Total"] = don_vs_time(start_timestamp, donation_data, len(dataframe))
+        print(dataframe)
 
 
 main()
